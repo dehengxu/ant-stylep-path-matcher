051ce7225bc5f6e403eff3b188369ac02172e221
sean398 <guohaochuan5461@gmail.com>
2022-06-21 14:59:32 +0000

    feature: add ant style path matcher

---
+++ readme.md
@@ -0,0 +1,33 @@
+# Ant-style Path Matcher
+
+An implementation for Ant-style path patterns.
+Part of this mapping code has been kindly borrowed from Apache Ant and Spring Framework.
+
+The mapping matches URLs using the following rules:
+
+? match one character
+
+- match zero or more characters
+  \*\* match zero or more directories in a path
+  {value} matches the regexp [a-z]+ as a path variable named "spring"
+
+## Install
+
+```
+ npm i ant-style-path-matcher
+```
+
+## Usage
+
+```
+import AntPathMatcher from 'ant-path-matcher'
+
+const matcher = new AntPathMatcher() ;
+
+matcher.antPathMatch('/path/**/?z','/path/x/y/z/yz')
+// true
+
+matcher.antPathMatch('/abc/{id}/efg','/abc/1/efg')
+// true
+
+```
---
+++ src/index.ts
@@ -0,0 +1,41 @@
+// ? match one character
+// * match zero or more characters
+// ** match zero or more directories
+// {string} matches the regexp [a-z]+ as a path variable
+
+/**
+ * @desc class of AntPathMatcher
+ */
+class AntPathMatcher {
+  private static getRegPattern = (patternSeg: string) => {
+    if (patternSeg === "*") return "[.]{0,}"; // match zero or more characters
+    if (patternSeg === "**") return "[.|/]{0,}"; // match zero or more derectories
+    let replacedReg = patternSeg;
+    if (replacedReg.includes("?")) {
+      // match one character
+      replacedReg = replacedReg.replace(/\?/g, "[\\w]");
+    }
+    if (replacedReg.includes("{") && replacedReg.includes("}")) {
+      // match {value}
+      replacedReg = replacedReg.replace(/{[\w]+}/g, "[\\w]+");
+    }
+
+    return replacedReg;
+  };
+
+  /**
+   * @desc match ant style path
+   * @param antPattern: string
+   * @param path: string
+   * @return boolean
+   */
+  public static antPathMatch = (antPattern: string, path: string) => {
+    const matchSeg = antPattern.split("/");
+    const matchRegs = matchSeg.map((e) => this.getRegPattern(e));
+    const regexString = matchRegs.join("/");
+    const regex = new RegExp(`^${regexString}$`);
+    return regex.test(path);
+  };
+}
+
+export default AntPathMatcher;
